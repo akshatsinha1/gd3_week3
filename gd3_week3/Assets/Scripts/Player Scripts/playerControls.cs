7df72e0b80a72c9013be39047481d5e0fb5a9d17
akshatsinha1 <akshat.sinha@techeducators.co.uk>
2024-08-12 13:17:30 +0100

    PLayer Controls Implemented

--- gd3_week3/Assets/Scripts/Player Scripts/playerControls.cs
+++ gd3_week3/Assets/Scripts/Player Scripts/playerControls.cs
@@ -6,6 +6,7 @@ public class playerControls : MonoBehaviour
 {
     public float moveSpeed = 10;
     float horizontal;
+    public float xRange;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,17 @@ public class playerControls : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
-
         transform.Translate(moveSpeed * Time.deltaTime * horizontal *Vector3.right );
+
+        if(transform.position.x >= xRange) // make sure the player doesnt go beyond it
+        {
+            transform.position = new Vector3(xRange, 0, transform.position.z);
+        }
+
+
+        if(transform.position.x <= -xRange) // make sure the player doesnt go beyond it
+        {
+            transform.position = new Vector3(-xRange, 0, transform.position.z);
+        }
     }
 }
